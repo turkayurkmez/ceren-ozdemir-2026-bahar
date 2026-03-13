534c8738d91553dc5b9fa0a59248219344508997
türkay ürkmez <turkay.urkmez@outlook.com>
2026-03-13 21:32:21 +0300

    Initial project setup with Hello World and config files
    Added .gitignore and .gitattributes for Visual Studio/.NET.
    Created solution and project files targeting .NET 10.0.
    Implemented a basic Program.cs with "Merhaba Dünya!" output and comment examples.

---
+++ ilkProjemiz/ilkProjemiz/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Merhaba Dünya!");
+
+//Bu biçimde dilediğiniz gibi notlar alabilirsiniz. Bu satır derleyici tarafından dikkate alınmaz. Sadece sizin için bir hatırlatma veya açıklama olarak kalır.
+
+/*
+ * 
+ * Bu da çok satırlı bir yorumdur. Yani birden fazla satırda not almak istediğinizde bu biçimi kullanabilirsiniz. Bu şekilde yazdığınız her şey derleyici tarafından dikkate alınmaz ve sadece sizin için bir hatırlatma veya açıklama olarak kalır.
+ */
+
+//Merhaba
